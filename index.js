155249b2329d6ba9872d771b9e743c0759e6c8c0
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2020-04-26 22:37:24 +0700

    [ADD] Added functionality for management of multiple

--- index.js
+++ index.js
@@ -5,6 +5,24 @@ const cheerio = require('cheerio');
 
 dotenv.config();
 const bot = new TeleBot(process.env.TOKEN);
+const users = [
+    {
+        link: 'almaz',
+        username: 'pheonix97al',
+    },
+    {
+        link: 'aibek',
+        username: 'dmndcrow',
+    },
+    {
+        link: 'nartay',
+        username: 'zhanybekovv',
+    },
+    {
+        link: 'sabyr',
+        username: 'megasaab',
+    }
+];
 
 async function getSolvedFromUsername (username) {
     const defaultData = await axios('https://leetcode.com/' + username)
@@ -29,56 +47,33 @@ async function getSolvedFromUsername (username) {
     return defaultData;
 }
 
+const capitalize = (s) => {
+    if (typeof s !== 'string') return '';
+    return s.charAt(0).toUpperCase() + s.slice(1)
+};
+
+ratingText = users.map(user => '/' + user.link + ' Rating of ' + capitalize(user.link) + '\n').join('');
+console.log(ratingText);
+
 bot.on(['/start'], (msg) => msg.reply.text(
     'Welcome!\n' +
-    '/rating Rating of Elite Boys\n' +
-    '/aibek Rating of Aibek\n' +
-    '/almaz Rating of Almaz\n' +
-    '/nartay Rating of Nartay'
+    ratingText
 ));
 
-bot.on(['/aibek'], (msg) => {
-    getSolvedFromUsername('dmndcrow').then(data =>{
-        const {name, username, solved, all, error} = data;
-        if (error) {
-            return msg.reply.text('Error is encountered: ' + error);
-        }
-        return msg.reply.text(
-            'Name: ' + name + '\n' +
-            'Username: ' + username + '\n' +
-            'Solved: ' + solved + '\n' +
-            'All: ' + all + '\n'
-        );
-    });
-});
-
-bot.on(['/almaz'], (msg) => {
-    getSolvedFromUsername('pheonix97al').then(data =>{
-        const {name, username, solved, all, error} = data;
-        if (error) {
-            return msg.reply.text('Error is encountered: ' + error);
-        }
-        return msg.reply.text(
-            'Name: ' + name + '\n' +
-            'Username: ' + username + '\n' +
-            'Solved: ' + solved + '\n' +
-            'All: ' + all + '\n'
-        );
-    });
-});
-
-bot.on(['/nartay'], (msg) => {
-    getSolvedFromUsername('zhanybekovv').then(data =>{
-        const {name, username, solved, all, error} = data;
-        if (error) {
-            return msg.reply.text('Error is encountered: ' + error);
-        }
-        return msg.reply.text(
-            'Name: ' + name + '\n' +
-            'Username: ' + username + '\n' +
-            'Solved: ' + solved + '\n' +
-            'All: ' + all + '\n'
-        );
+users.forEach(user => {
+    bot.on(['/' + user.link], (msg) => {
+        getSolvedFromUsername(user.username).then(data =>{
+            const {name, username, solved, all, error} = data;
+            if (error) {
+                return msg.reply.text('Error is encountered: ' + error);
+            }
+            return msg.reply.text(
+                'Name: ' + name + '\n' +
+                'Username: ' + username + '\n' +
+                'Solved: ' + solved + '\n' +
+                'All: ' + all + '\n'
+            );
+        });
     });
 });
 
