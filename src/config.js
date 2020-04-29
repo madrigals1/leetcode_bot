90401a847e8347b1e8783b4a8851fa2f350356fb
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2020-04-29 12:21:39 +0700

    [ADD] Added docker env variable for mongo

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const dotenv = require("dotenv");
 dotenv.config();
 const token = process.env.TOKEN;
-const mongoURL = process.env.MONGO_URL || 'localhost';
+const mongoURL = process.env.MONGO_URL ? 'mongo' : 'localhost';
 
 module.exports = {token, mongoURL};
