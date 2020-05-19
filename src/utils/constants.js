c25d6da4c842235424c9358cc172dbfd15476c81
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2020-05-19 21:16:39 +0700

    [FIX] Fixed importing one by one for constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,11 +3,13 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 // ENV Variables
-const { TELEGRAM_TOKEN } = process.env;
-const { DB_NAME } = process.env;
-const { DB_PORT } = process.env;
-const { LEETCODE_URL } = process.env;
-const { MONGO_URL } = process.env;
+const {
+  TELEGRAM_TOKEN,
+  DB_NAME,
+  DB_PORT,
+  LEETCODE_URL,
+  MONGO_URL,
+} = process.env;
 
 // Static variables
 const welcomeMessage = `Welcome! This is Leetcode Rating bot Elite Boys.
