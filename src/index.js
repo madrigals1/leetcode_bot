7b8693dc9911d276327c5632f306442e6ef3d7ed
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2020-06-07 16:31:15 +0700

    [FIX] Fixed issue with incorrect startup

--- src/index.js
+++ src/index.js
@@ -5,19 +5,20 @@ const Listener = require('./models/listener');
 const { connect } = require('./models/database');
 const { refresh } = require('./models/user');
 
-// Adding listeners after refreshing the users
-refresh()
-  .then(() => {
-    listeners.forEach((listener) => {
-      new Listener(listener.types, listener.callback).init(bot);
-    });
-  });
-
 // Connecting to Database
 connect();
 
-// Starting the bot
-bot.start();
+// Refreshing the users
+refresh()
+  .then(() => {
+    // Adding listeners for the bot
+    listeners.forEach((listener) => {
+      new Listener(listener.types, listener.callback).init(bot);
+    });
 
-// Starting the scheduler for database refresher
-startScheduler();
+    // Starting the bot
+    bot.start();
+
+    // Starting the scheduler for database refresher
+    startScheduler();
+  });
