7ac59f7b5cdf4617856693da4edb787f9acd5e7b
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2020-06-28 14:59:54 +0700

    [FIX] fixed issues with console.log and eslint

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,16 +1,18 @@
 const moment = require('moment');
 
 function log(...args) {
+  // eslint-disable-next-line no-console
   console.log(...args);
 }
 
 function error(...args) {
+  // eslint-disable-next-line no-console
   console.error(...args);
 }
 
 const refreshLog = () => {
   const date = moment().format('YYYY-MM-DD hh:mm a');
-  console.log(`Database is refreshed ${date}`);
+  log(`Database is refreshed ${date}`);
 };
 
 module.exports = { log, error, refreshLog };
