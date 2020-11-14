00286c37941900e1d3f4d2f955b5bf19be01c20a
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2020-11-14 15:32:14 +0700

    FIX error with users table not being present

--- src/database/sqlite3/index.js
+++ src/database/sqlite3/index.js
@@ -28,6 +28,9 @@ class SQLite {
     // Set database from connection
     this.database = connection.success ? connection.database : null;
 
+    // Create users table after connection
+    await this.createUserTable();
+
     return new Promise((resolve, reject) => {
       if (connection.success) {
         resolve(true);
