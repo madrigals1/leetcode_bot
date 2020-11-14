6a7e844d4e74b22cbe9b36d69334704598eb441f
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2020-11-14 16:13:23 +0700

    FIX issue with newest DB not having users model + Add timeout

--- src/database/sqlite3/index.js
+++ src/database/sqlite3/index.js
@@ -9,39 +9,36 @@ const QUERIES = require('./queries');
 class SQLite {
   // Connect to Database
   async connect() {
-    // Create database connection
-    const connection = await open({
-      filename: 'leetbot.db',
-      driver: sqlite3.Database,
-    })
-      .then((database) => {
-        // Log that database is connected
-        log(SERVER_MESSAGES.CONNECTION_STATUS.SUCCESSFUL);
-        return { success: true, database };
-      })
-      .catch((err) => {
-        // Log that database connection had errors
-        log(SERVER_MESSAGES.CONNECTION_STATUS.ERROR(err));
-        return { success: false, err };
-      });
-
-    // Set database from connection
-    this.database = connection.success ? connection.database : null;
-
-    // Create users table after connection
-    await this.createUserTable();
-
     return new Promise((resolve, reject) => {
-      if (connection.success) {
-        resolve(true);
-      }
-      reject(Error(connection.err));
+      // Create database connection
+      open({
+        filename: 'src/database/sqlite3/leetbot.db',
+        driver: sqlite3.Database,
+      })
+        .then((database) => {
+          // Log that database is connected
+          log(SERVER_MESSAGES.CONNECTION_STATUS.SUCCESSFUL);
+
+          // Set database object
+          this.database = database;
+
+          // Create database table
+          this.createUserTable();
+
+          resolve(true);
+        })
+        .catch((err) => {
+          // Log that database connection had errors
+          log(SERVER_MESSAGES.CONNECTION_STATUS.ERROR(err));
+
+          reject(Error(err));
+        });
     });
   }
 
   // Create Users table if not exists
   createUserTable() {
-    this.database.run(QUERIES.CREATE_USERS_TABLE);
+    return this.database.run(QUERIES.CREATE_USERS_TABLE);
   }
 
   // Find all Users
--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ const startScheduler = require('./utils/scheduler');
 const Database = require('./database');
 const User = require('./cache/user');
 const { TELEGRAM, DISCORD } = require('./utils/constants');
+const { delay } = require('./utils/helper');
 
 // Connecting to Database
-Database.connect().then(() => {
+Database.connect().then(async () => {
+  // Wait 1 second for Databases to initialize
+  await delay(1000);
+
   // Refreshing the users
   User.refresh()
     .then(() => {
