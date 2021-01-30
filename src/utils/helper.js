7fce3c8b02cd2f7e74b8ede93acffc4e661b08ab
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2021-01-30 22:56:33 +0700

    ADD tests for utils/helper functions

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -6,7 +6,9 @@ const error = (...args) => console.error(...args);
 
 const delay = (msTime) => new Promise((res) => setTimeout(res, msTime));
 
-const isTrue = (value) => ['true', 'True', '1', 't', 'T'].includes(value);
+const isTrue = (value) => (
+  ['true', 'True', '1', 't', 'T', 1, true].includes(value)
+);
 
 module.exports = {
   log, error, delay, isTrue,
