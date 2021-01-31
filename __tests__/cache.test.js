deb274c08891f780829cee65e693a41a2de4fc73
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2021-01-31 21:05:24 +0700

    ADD tests for Cache.refreshUsers

--- __tests__/cache.test.js
+++ __tests__/cache.test.js
@@ -1,12 +1,14 @@
-// import { jest } from '@jest/globals';
+import { jest } from '@jest/globals';
 
 import Cache from '../src/cache';
 
 import getLeetcodeDataFromUsername from './__mocks__/utils';
 import MockDatabaseProvider from './__mocks__/database';
+import users from './__mocks__/data';
 
 Cache.database = new MockDatabaseProvider();
 Cache.getLeetcodeDataFromUsername = getLeetcodeDataFromUsername;
+Cache.delayTime = 0;
 
 beforeEach(async () => {
   await Cache.clearUsers();
@@ -55,4 +57,28 @@ test('Cache.addOrReplaceUserInCache method', async () => {
     .toBe(JSON.stringify(secondUserData));
 
   expect(Cache.amount).toBe(1);
+});
+
+test('Cache.refreshUsers method', async () => {
+  // eslint-disable-next-line no-console
+  console.log = jest.fn();
+
+  // Save original array
+  const usersClone = [...users];
+
+  // Adding in this order, because they will be sorted by solved count
+  await Cache.addUser('random_username_2');
+  await Cache.addUser('random_username');
+
+  users[0].name = 'New Name 1';
+  users[1].name = 'New Name 2';
+
+  await Cache.refreshUsers();
+
+  expect(Cache.users[0].name).toBe('New Name 2');
+  expect(Cache.users[1].name).toBe('New Name 1');
+
+  // Clear array and bring back original array
+  users.length = 0;
+  usersClone.forEach((user) => users.push(user));
 });
