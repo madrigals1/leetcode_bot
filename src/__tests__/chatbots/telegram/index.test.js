0405f5b3e6c2c02a1a26475b61725c14b1c48990
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2021-02-14 22:09:10 +0700

    ADD test for telegram

---
+++ src/__tests__/chatbots/telegram/index.test.js
@@ -0,0 +1,31 @@
+import { jest } from '@jest/globals';
+
+import TelegramBotInstance from '../../../chatbots/telegram';
+import constants from '../../../utils/constants';
+import dictionary from '../../../utils/dictionary';
+
+TelegramBotInstance.token = constants.TELEGRAM.TOKEN;
+
+beforeAll(async () => {
+  jest.setTimeout(30000);
+});
+
+afterAll(async () => {
+  jest.setTimeout(5000);
+});
+
+test('discord.index.run function', async () => {
+  expect(TelegramBotInstance.token).toBe(constants.TELEGRAM.TEST_TOKEN);
+
+  await TelegramBotInstance.run();
+
+  // eslint-disable-next-line no-console
+  expect(console.log).toHaveBeenCalledWith(
+    dictionary.SERVER_MESSAGES.TELEGRAM_BOT_IS_CONNECTED,
+  );
+
+  // eslint-disable-next-line no-console
+  expect(console.log).toHaveBeenCalledWith(
+    dictionary.SERVER_MESSAGES.TELEGRAM_BOT_IS_RUNNING,
+  );
+});
