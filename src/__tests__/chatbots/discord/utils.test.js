e283f8435971f7bd7d2acd2dda09b6727a1f3d40
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2021-02-15 21:28:06 +0700

    fix test for utils/discord now using mock class

--- src/__tests__/chatbots/discord/utils.test.js
+++ src/__tests__/chatbots/discord/utils.test.js
@@ -1,11 +1,8 @@
 import { formatMessage, reply } from '../../../chatbots/discord/utils';
 import { isPromise } from '../../../utils/helper';
+import MockFuncDiscord from '../../__mocks__/chatbots.mock';
 
-let formattedMessage = null;
-
-const send = (message) => {
-  formattedMessage = message;
-};
+const mockFuncDiscord = new MockFuncDiscord();
 
 test('chatbots.discord.utils.formatMessage function', async () => {
   const testCases = [
@@ -28,28 +25,41 @@ test('chatbots.discord.utils.reply function', async () => {
   const testCases = [
     {
       message: '<b>Message 1</b>',
-      context: { channel: { send }, photoUrl: 'random_url' },
+      context: {
+        channel: { send: MockFuncDiscord.send }, photoUrl: 'random_url',
+      },
       expected: 'Success',
       expectedMessage: '**Message 1**',
+      expectedOptions: { files: ['random_url'] },
     },
     {
       message: '<i>Message 2</i>',
-      context: { photoUrl: 'random_url_2' },
+      context: { channel: { send: MockFuncDiscord.send } },
+      expected: 'Success',
+      expectedMessage: '**Message 2**',
+      expectedOptions: {},
+    },
+    {
+      message: '<i>Message 3</i>',
+      context: { photoUrl: 'random_url_3' },
       expected: Error('Channel is not provided in context'),
       expectedMessage: null,
+      expectedOptions: {},
     },
   ];
 
   testCases.forEach(async ({
-    message, context, expected, expectedMessage,
+    message, context, expected, expectedMessage, expectedOptions,
   }) => {
     const promise = reply(message, context);
     expect(isPromise(promise)).toBe(true);
 
     const result = await promise;
-    expect(result).toBe(expected);
-    expect(expectedMessage).toBe(formattedMessage);
 
-    formattedMessage = null;
+    expect(result).toBe(expected);
+    expect(expectedMessage).toBe(mockFuncDiscord.message);
+    expect(expectedOptions).toBe(mockFuncDiscord.expectedOptions);
+
+    mockFuncDiscord.send(null);
   });
 });
