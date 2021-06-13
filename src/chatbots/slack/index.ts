a7d58d86659f72a9e6b38038b47dc3b3918d8715
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2021-06-13 21:45:31 +0700

    ADD simple message handling

---
+++ src/chatbots/slack/index.ts
@@ -0,0 +1,34 @@
+import * as SlackBot from 'slackbots';
+
+import constants from '../../utils/constants';
+import dictionary from '../../utils/dictionary';
+import { log } from '../../utils/helper';
+
+import createBot from './bot';
+
+class Slack {
+  token: string = constants.DISCORD.TOKEN;
+
+  bot: SlackBot.Client;
+
+  async run() {
+    // Create Bot with token
+    this.bot = await createBot(this.token);
+
+    this.bot.on('start', () => {
+      const params = {
+        icon_emoji: ':robot_face:',
+      };
+
+      this.bot.postMessageToChannel(
+        'Hello',
+        'LeetCode BOT is working correctly!',
+        params,
+      );
+    });
+
+    log(dictionary.SERVER_MESSAGES.DISCORD_BOT_IS_RUNNING);
+  }
+}
+
+export default new Slack();
