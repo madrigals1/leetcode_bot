f08751a72cf5c636d87f24297ee067ce03356e0a
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2021-07-26 01:51:10 +0700

    ADD remove action

--- src/__tests__/chatbots/actions.test.ts
+++ src/__tests__/chatbots/actions.test.ts
@@ -3,13 +3,21 @@ import dictionary from '../../utils/dictionary';
 import Cache from '../../cache';
 import getLeetcodeDataFromUsername from '../__mocks__/utils.mock';
 import MockDatabaseProvider from '../__mocks__/database.mock';
+import constants from '../../utils/constants';
 
 const mockbot = new Mockbot();
 Cache.database = new MockDatabaseProvider();
 Cache.getLeetcodeDataFromUsername = getLeetcodeDataFromUsername;
 Cache.delayTime = 0;
 
-beforeEach(() => mockbot.clear());
+const mockPassword = 'random_password';
+constants.MASTER_PASSWORD = mockPassword;
+
+beforeEach(() => {
+  mockbot.clear();
+  Cache.clearUsers();
+  Cache.userLimit = parseInt(constants.USER_AMOUNT_LIMIT, 10);
+});
 
 test('chatbots.actions.start action', async () => {
   // Test with correct arguments
@@ -35,6 +43,8 @@ test('chatbots.actions.add action', async () => {
   const message2 = dictionary.BOT_MESSAGES.USERNAME_WAS_ADDED(
     'random_username_2', Cache.userAmount, Cache.userLimit,
   );
+
+  expect(Cache.users.length).toBe(2);
 
   expect(mockbot.lastMessage()).toEqual(`User List:\n${message1}${message2}`);
 
@@ -83,6 +93,48 @@ test('chatbots.actions.refresh action', async () => {
 
   // Test with incorrect arguments
   await mockbot.send('/refresh excess_arg');
+
+  expect(mockbot.lastMessage())
+    .toEqual(dictionary.BOT_MESSAGES.INCORRECT_INPUT);
+});
+
+test('chatbots.actions.remove action', async () => {
+  // Test with correct arguments (Without username)
+  await mockbot.send(`/remove ${mockPassword}`);
+
+  expect(mockbot.lastMessage()).toBe(dictionary.BOT_MESSAGES.USER_LIST_REMOVE);
+  /* TODO: Check buttons */
+
+  // Test with correct arguments (With username)
+  await mockbot.send('/add random_username');
+
+  await mockbot.send(`/remove random_username ${mockPassword}`);
+  const messages = mockbot.messages(2);
+
+  expect(messages[0])
+    .toBe(dictionary.BOT_MESSAGES.USERNAME_WILL_BE_DELETED('random_username'));
+  expect(messages[1])
+    .toBe(dictionary.BOT_MESSAGES.USERNAME_WAS_DELETED('random_username'));
+
+  // Test with incorrect arguments (Username doesn't exist)
+  await mockbot.send(`/remove not_existing_username ${mockPassword}`);
+
+  expect(mockbot.lastMessage())
+    .toBe(dictionary.BOT_MESSAGES.USERNAME_NOT_FOUND('not_existing_username'));
+
+  // Test with incorrect arguments (Incorrect password)
+  await mockbot.send('/remove blablabla incorrect_password');
+
+  expect(mockbot.lastMessage())
+    .toBe(dictionary.BOT_MESSAGES.PASSWORD_IS_INCORRECT);
+
+  // Test with incorrect arguments (Arguments count)
+  await mockbot.send('/remove');
+
+  expect(mockbot.lastMessage())
+    .toEqual(dictionary.BOT_MESSAGES.INCORRECT_INPUT);
+
+  await mockbot.send('/remove 123 123 123');
 
   expect(mockbot.lastMessage())
     .toEqual(dictionary.BOT_MESSAGES.INCORRECT_INPUT);
