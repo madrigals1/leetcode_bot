6d1232f0a60f46dc4223ce2625058e925947ba61
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2021-07-25 19:17:32 +0700

    ADD test case for isValidArgsCount

--- src/__tests__/chatbots/decorators/utils.test.ts
+++ src/__tests__/chatbots/decorators/utils.test.ts
@@ -1,4 +1,4 @@
-import { getArgs } from '../../../chatbots/decorators/utils';
+import { getArgs, isValidArgsCount } from '../../../chatbots/decorators/utils';
 import { Args } from '../../../chatbots/models';
 
 test('chatbots.decorators.utils.getArgs function', async () => {
@@ -20,5 +20,117 @@ test('chatbots.decorators.utils.getArgs function', async () => {
   testCases.forEach(({ message, expectedArgs }) => {
     const args: string[] = getArgs(message);
     expect(args).toStrictEqual(expectedArgs);
+  });
+});
+
+test('chatbots.decorators.utils.isValidArgsCount function', async () => {
+  const testCases = [
+    {
+      input: {
+        args: [],
+        argsCount: [0],
+      },
+      output: true,
+    },
+    {
+      input: {
+        args: ['asd'],
+        argsCount: [0, 1],
+      },
+      output: true,
+    },
+    {
+      input: {
+        args: [],
+        argsCount: [0, 1],
+      },
+      output: true,
+    },
+    {
+      input: {
+        args: ['asd', 'asd'],
+        argsCount: [0, 1],
+      },
+      output: false,
+    },
+    {
+      input: {
+        args: ['asd', 'asd', 'asd'],
+        argsCount: [3],
+      },
+      output: true,
+    },
+    {
+      input: {
+        args: ['asd', 'asd', 'asd', 'asd'],
+        argsCount: [3],
+      },
+      output: false,
+    },
+    {
+      input: {
+        args: ['asd', 'asd', 'asd'],
+        argsCount: [0, 1],
+      },
+      output: false,
+    },
+    {
+      input: {
+        args: ['asd', 'asd', 'asd'],
+        argsCount: '+',
+      },
+      output: true,
+    },
+    {
+      input: {
+        args: ['asd', 'asd', 'asd', 'asd', 'asd', 'asd', 'asd'],
+        argsCount: '+',
+      },
+      output: true,
+    },
+    {
+      input: {
+        args: [],
+        argsCount: '+',
+      },
+      output: false,
+    },
+    {
+      input: {
+        args: [],
+        argsCount: '?',
+      },
+      output: true,
+    },
+    {
+      input: {
+        args: ['asd', 'asd', 'asd'],
+        argsCount: '?',
+      },
+      output: true,
+    },
+    {
+      input: {
+        args: ['asd', 'asd', 'asd'],
+        argsCount: '?',
+      },
+      output: true,
+    },
+    {
+      input: {
+        args: ['asd', 'asd', 'asd'],
+        argsCount: 'X',
+      },
+      output: Error('Invalid argument argsCount: X'),
+    },
+  ];
+
+  testCases.forEach(({ input, output }) => {
+    const { args, argsCount } = input;
+    if (typeof output === 'boolean') {
+      expect(isValidArgsCount(args, argsCount)).toStrictEqual(output);
+    } else {
+      expect(() => isValidArgsCount(args, argsCount)).toThrowError(output);
+    }
   });
 });
