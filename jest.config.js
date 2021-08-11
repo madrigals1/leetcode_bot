b80b9bfdcd6fff041ab04bf86f83357d4d6c7729
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2021-08-12 00:28:53 +0600

    FIX put coverageThreshold in correct place

--- jest.config.js
+++ jest.config.js
@@ -10,4 +10,12 @@ module.exports = {
   modulePathIgnorePatterns: filesToIgnore,
   testEnvironment: 'node',
   transform: { '\\.ts$': ['ts-jest'] },
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
+    },
+  },
 };
