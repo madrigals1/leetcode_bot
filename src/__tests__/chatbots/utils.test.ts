1eaae9624833b918a7f7f47f507ef95ea6aa9963
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2021-09-25 00:56:44 +0700

    ADD test case for compareUser

---
+++ src/__tests__/chatbots/utils.test.ts
@@ -0,0 +1,57 @@
+import { user1 } from '../__mocks__/data.mock';
+import { getCompareDataFromUser } from '../../chatbots/utils';
+
+beforeEach(() => {
+  //
+});
+
+test('chatbots.utils.getCompareDataFromUser action', async () => {
+  // Get compare data from user
+  const compareData = getCompareDataFromUser(user1);
+
+  // Compare fields
+  expect(compareData.image).toBe(user1.profile.userAvatar);
+
+  compareData.bio_fields.forEach(({ name, value }) => {
+    switch (name) {
+      case 'Name':
+        expect(value).toBe(user1.name);
+        break;
+      case 'Username':
+        expect(value).toBe(user1.username);
+        break;
+      case 'Location':
+        expect(value).toBe(user1.profile.countryName);
+        break;
+      case 'Company':
+        expect(value).toBe(user1.profile.company);
+        break;
+
+      default:
+        break;
+    }
+  });
+
+  compareData.compare_fields.forEach(({ name, value }) => {
+    switch (name) {
+      case 'Problems Solved':
+        expect(value).toBe(user1.solved);
+        break;
+      case 'Contest Rating':
+        expect(value)
+          .toBe(Math.round(user1.contestData.userContestRanking?.rating));
+        break;
+      case 'Location':
+        expect(value).toBe(user1.submitStats.totalSubmissionNum[0].submissions);
+        break;
+      case 'Company':
+        expect(value).toBe(user1.contributions.points);
+        break;
+
+      default:
+        break;
+    }
+  });
+
+  expect(compareData.image).toBe(user1.profile.userAvatar);
+});
