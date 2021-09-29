c37f1797f99211926daccd9a1f1374fd8fd97c96
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2021-09-29 20:43:16 +0700

    FIX failing tests for Status map

--- src/__tests__/__mocks__/data.mock.ts
+++ src/__tests__/__mocks__/data.mock.ts
@@ -29,14 +29,14 @@ export const user1: User = {
   submissions: [
     {
       link: getLeetcodeProblemLink('random_problem_slug'),
-      status: constants.STATUS_MAP.Accepted,
+      status: constants.SUBMISSION_STATUS_MAP.Accepted,
       language: 'cpp',
       name: 'Random Problem Name',
       time: '10 minutes ago',
     },
     {
       link: getLeetcodeProblemLink('random_problem_slug_2'),
-      status: constants.STATUS_MAP['Runtime Error'],
+      status: constants.SUBMISSION_STATUS_MAP['Runtime Error'],
       language: 'python',
       name: 'Random Problem Name 2',
       time: '2 weeks ago',
@@ -120,14 +120,14 @@ export const user2: User = {
   submissions: [
     {
       link: getLeetcodeProblemLink('random_problem_slug_3'),
-      status: constants.STATUS_MAP.Accepted,
+      status: constants.SUBMISSION_STATUS_MAP.Accepted,
       language: 'java',
       name: 'Random Problem Name 3',
       time: '2 hours ago',
     },
     {
       link: getLeetcodeProblemLink('random_problem_slug_4'),
-      status: constants.STATUS_MAP['Runtime Error'],
+      status: constants.SUBMISSION_STATUS_MAP['Runtime Error'],
       language: 'javascript',
       name: 'Random Problem Name 4',
       time: '9 hours ago',
