b2e06336df44da1a716f686af729246ecd93f906
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2021-09-29 20:58:44 +0700

    Rename NODE_SCHEDULE_TIME -> USERS_REFRESH_DELAY

--- src/utils/scheduler.ts
+++ src/utils/scheduler.ts
@@ -6,7 +6,7 @@ import constants from './constants';
 
 export function startScheduler(): void {
   schedule.scheduleJob(
-    constants.SYSTEM.NODE_SCHEDULE_TIME,
+    constants.SYSTEM.USERS_REFRESH_DELAY,
     () => Cache.refreshUsers(),
   );
 }
