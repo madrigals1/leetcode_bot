cde99e358c0fa1379ddb5bff406c900be73ee3fb
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2021-10-02 08:13:42 +0700

    FIX issues with new version of Discord.js

--- src/chatbots/discord/bot.ts
+++ src/chatbots/discord/bot.ts
@@ -1,11 +1,11 @@
-import * as DiscordBot from 'discord.js';
+import { Client, Intents } from 'discord.js';
 
 import { log } from '../../utils/helper';
 import dictionary from '../../utils/dictionary';
 
-const createBot = async (token: string): Promise<DiscordBot.Client> => {
+const createBot = async (token: string): Promise<Client> => {
   // Create bot and use Token to Login
-  const bot = new DiscordBot.Client();
+  const bot = new Client({ intents: [Intents.FLAGS.GUILDS] });
   await bot.login(token);
 
   log(dictionary.SERVER_MESSAGES.DISCORD_BOT_IS_CONNECTED);
