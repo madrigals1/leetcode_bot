05d96693e7e9dc1453d1b118323c706d83edca16
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2021-10-14 22:12:00 +0700

    ADD interface for ParsedArgument

--- src/chatbots/decorators/models.ts
+++ src/chatbots/decorators/models.ts
@@ -1,3 +1,10 @@
+export interface ParsedArgument {
+  index: number;
+  key: string;
+  name?: string;
+  value: string | string[];
+}
+
 export interface ActionContext {
   name: string,
   argsCount: number[] | string,
