386532c21660519d446c73f8e9f17c3a0032adbf
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2021-10-16 16:58:02 +0700

    Update interfaces in test cases

--- src/__tests__/__mocks__/models/mockData.model.ts
+++ src/__tests__/__mocks__/models/mockData.model.ts
@@ -1,4 +1,4 @@
-import { Argument, ParsedArgument } from '../../../chatbots/decorators/models';
+import { Argument, IParsedArgument } from '../../../chatbots/decorators/models';
 import { User } from '../../../leetcode/models';
 
 export interface MockDatabaseInterface {
@@ -12,14 +12,21 @@ export interface OtherModel {
   field: string;
 }
 
+export interface ArgumentTestCaseParsedArgument extends IParsedArgument {
+  index: number;
+  key: string;
+  name: string;
+  _value: string | string[];
+}
+
 export interface ArgumentTestCase {
   input: {
     providedArgs: string[],
     requestedArgs: Argument[],
   }
   output: {
-    byKey: Record<string, ParsedArgument>,
-    byIndex: Record<number, ParsedArgument>,
+    byKey: Record<string, ArgumentTestCaseParsedArgument>,
+    byIndex: Record<number, ArgumentTestCaseParsedArgument>,
   }
   error?: Error,
 }
