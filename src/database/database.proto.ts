f6670d86093e22f259a23aea6929ad55d7a2bee3
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2021-11-03 22:20:01 +0700

    Add proto functionality for Subscription

--- src/database/database.proto.ts
+++ src/database/database.proto.ts
@@ -1,5 +1,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
+
+import { Subscription } from '../leetcode/models';
+
 /* eslint-disable class-methods-use-this */
 class DatabaseProvider {
   isRefreshing = false;
@@ -31,6 +34,18 @@ class DatabaseProvider {
   }
 
   async removeAllUsers(): Promise<any> {
+    throw new Error('Not Implemented');
+  }
+
+  async upsertSubscription(subscription: Subscription): Promise<any> {
+    throw new Error('Not Implemented');
+  }
+
+  async removeSubscription(chatId: string): Promise<any> {
+    throw new Error('Not Implemented');
+  }
+
+  async getAllSubscriptions(): Promise<any> {
     throw new Error('Not Implemented');
   }
 }
