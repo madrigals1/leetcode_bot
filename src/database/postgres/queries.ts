424793982258fbf2c01a5f5a807d2483a3a861f2
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2021-11-04 01:09:42 +0700

    Add queries for postgres

--- src/database/postgres/queries.ts
+++ src/database/postgres/queries.ts
@@ -1,3 +1,5 @@
+import { Subscription } from '../../leetcode/models';
+
 const QUERIES = {
   CREATE_USERS_TABLE: 'CREATE TABLE IF NOT EXISTS users (username varchar);',
   GET_ALL_USERS: 'SELECT username FROM users;',
@@ -5,6 +7,21 @@ const QUERIES = {
   ADD_USER: (username: string): string => `INSERT INTO users (username) VALUES ('${username}');`,
   REMOVE_USER: (username: string): string => `DELETE FROM users WHERE username='${username}';`,
   REMOVE_ALL_USERS: 'DELETE FROM users;',
+  CREATE_SUBSCRIPTIONS_TABLE: 'CREATE TABLE IF NOT EXISTS subscriptions '
+    + '(chatId varchar PRIMARY KEY, provider varchar);',
+  GET_ALL_SUBSCRIPTIONS: 'SELECT chatId, provider FROM subscriptions;',
+  LOAD_SUBSCRIPTION: (
+    chatId: string,
+  ): string => `SELECT chatId, provider FROM subscriptions WHERE chatId='${chatId}';`,
+  ADD_SUBSCRIPTION: (
+    subscription: Subscription,
+  ): string => `INSERT INTO subscriptions (chatId, provider) VALUES ('${subscription.chatId}', '${subscription.provider}');`,
+  UPDATE_SUBSCRIPTION: (
+    subscription: Subscription,
+  ): string => `UPDATE subscriptions SET provider='${subscription.provider}' WHERE chatId=${subscription.chatId};`,
+  REMOVE_SUBSCRIPTION: (
+    chatId: string,
+  ): string => `DELETE FROM subscriptions WHERE chatId='${chatId}';`,
 };
 
 export default QUERIES;
