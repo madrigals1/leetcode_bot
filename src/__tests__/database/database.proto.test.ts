5f651047bcefd685adfa553930ebdb9a36f70d82
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2022-01-05 00:24:28 +0700

    Add test case for database.proto

---
+++ src/__tests__/database/database.proto.test.ts
@@ -0,0 +1,25 @@
+import ProtoDatabase from '../../database/database.proto';
+
+test('database.proto class', async () => {
+  const database = new ProtoDatabase();
+  const errorMessage = 'Not Implemented';
+
+  await expect(database.connect())
+    .rejects
+    .toThrowError(new Error(errorMessage));
+  await expect(database.findAllUsers())
+    .rejects
+    .toThrowError(new Error(errorMessage));
+  await expect(database.loadUser('asd'))
+    .rejects
+    .toThrowError(new Error(errorMessage));
+  await expect(database.addUser('asd'))
+    .rejects
+    .toThrowError(new Error(errorMessage));
+  await expect(database.removeUser('asd'))
+    .rejects
+    .toThrowError(new Error(errorMessage));
+  await expect(database.removeAllUsers())
+    .rejects
+    .toThrowError(new Error(errorMessage));
+});
