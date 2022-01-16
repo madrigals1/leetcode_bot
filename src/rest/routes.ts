003441c276fb0cdd8d1c51f075a6fb373eb75e10
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2022-01-16 10:51:19 +0700

    Add default route for REST API

--- src/rest/routes.ts
+++ src/rest/routes.ts
@@ -3,11 +3,17 @@ import {
 } from 'restana';
 import { register } from 'prom-client';
 
+type rq = Request<Protocol>
+type rs = Response<Protocol>
+
 export function addRoutes(app: Service<Protocol.HTTP>): void {
+  // Default route
+  app.get('/', async (req: rq, res: rs) => {
+    res.send('LeetCode BOT - REST API');
+  });
+
   // Grafana metrics
-  app.get('/metrics', async (
-    req: Request<Protocol>, res: Response<Protocol>,
-  ) => {
+  app.get('/metrics', async (req: rq, res: rs) => {
     try {
       res.setHeader('Content-Type', register.contentType);
       res.send(await register.metrics());
