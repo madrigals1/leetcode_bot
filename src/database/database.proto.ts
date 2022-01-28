560533c87f4c55e543e1907235d2aca6ce8a2741
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2022-01-28 20:43:35 +0700

    Add simple documentation for Database Proto

--- src/database/database.proto.ts
+++ src/database/database.proto.ts
@@ -1,9 +1,14 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable class-methods-use-this */
 
 import dayjs from 'dayjs';
 
-/* eslint-disable class-methods-use-this */
+/**
+ * The DatabaseProvider class is a class that provides an interface to the
+ * database. It has a number of methods that are used to interact with the
+ * database.
+ */
 class DatabaseProvider {
   isRefreshing = false;
 
@@ -11,26 +16,49 @@ class DatabaseProvider {
 
   lastRefreshTime: dayjs.Dayjs;
 
+  /**
+   * Connect to the database and return a promise that resolves to the
+   * connection object.
+   */
   async connect(): Promise<any> {
     throw new Error('Not Implemented');
   }
 
+  /**
+   * Find all users.
+   */
   async findAllUsers(): Promise<any> {
     throw new Error('Not Implemented');
   }
 
+  /**
+   * Load a user from the database.
+   * The function signature is:
+   * @param {string} username - The username of the user to load.
+   */
   async loadUser(username: string): Promise<any> {
     throw new Error('Not Implemented');
   }
 
+  /**
+   * Add a user to the database.
+   * @param {string} username - The username of the user to add.
+   */
   async addUser(username: string): Promise<any> {
     throw new Error('Not Implemented');
   }
 
+  /**
+   * Remove a user from the database.
+   * @param {string} username - The username of the user to remove.
+   */
   async removeUser(username: string): Promise<any> {
     throw new Error('Not Implemented');
   }
 
+  /**
+   * Remove all users from the database.
+   */
   async removeAllUsers(): Promise<any> {
     throw new Error('Not Implemented');
   }
