e3bfa49645f7ad18a6d56cdb87ad91bba60c03ba
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2022-01-28 20:14:33 +0700

    Add lastRefreshTime to Database Proto

--- src/database/database.proto.ts
+++ src/database/database.proto.ts
@@ -1,10 +1,15 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
+
+import dayjs from 'dayjs';
+
 /* eslint-disable class-methods-use-this */
 class DatabaseProvider {
   isRefreshing = false;
 
   providerName = 'prototype';
+
+  lastRefreshTime: dayjs.Dayjs;
 
   async connect(): Promise<any> {
     throw new Error('Not Implemented');
