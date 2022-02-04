ccd63540dec4f93dc6e7dc0c317ee74527bb9da6
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2022-02-05 00:25:47 +0600

    Add ChannelKey to Context

--- src/chatbots/models/context.model.ts
+++ src/chatbots/models/context.model.ts
@@ -11,6 +11,7 @@ import {
 import TelegramBot from 'node-telegram-bot-api';
 
 import { ChatbotProvider } from '..';
+import { ChannelKey } from '../../cache/models/channel.model';
 import ArgumentManager from '../argumentManager';
 import { Argument } from '../decorators/models';
 
@@ -47,6 +48,7 @@ export interface Context {
   bot?: Client | TelegramBot;
   photoUrl?: string;
   password?: string;
+  channelKey?: ChannelKey;
   // Discord
   discordProvidedArguments?: readonly CommandInteractionOption[];
   interaction?: ComplexInteraction;
