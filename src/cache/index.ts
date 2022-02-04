7a04b32da1dc9d0166bd35a54d97a75dbd6a1a93
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2022-02-04 18:05:53 +0600

    Remove methods from Cache, that are moved to Channel Cache

--- src/cache/index.ts
+++ src/cache/index.ts
@@ -9,12 +9,13 @@ import dictionary from '../utils/dictionary';
 import { User } from '../leetcode/models';
 
 import { CacheResponse } from './models/response.model';
+import { Channel } from './channel';
 
 const { DATE_FORMAT } = constants.SYSTEM;
 const { SERVER_MESSAGES: SM, BOT_MESSAGES: BM } = dictionary;
 
 class Cache {
-  users: User[] = [];
+  channels: Channel[] = [];
 
   database = Database;
 
@@ -28,7 +29,7 @@ class Cache {
 
   // Return all users
   allUsers(): User[] {
-    return this.users;
+    return this.channels.flatMap((channel) => channel.users);
   }
 
   // Get amount of users
@@ -36,23 +37,12 @@ class Cache {
     return this.allUsers().length;
   }
 
-  // Replace User with username in the cache
-  addOrReplaceUserInCache(username: string, userData: User): void {
-    for (let i = 0; i < this.userAmount; i++) {
-      if (this.users[i].username.toLowerCase() === username.toLowerCase()) {
-        this.users[i] = userData;
-        return;
-      }
-    }
-
-    // If user was not found in cache, add user
-    this.users.push(userData);
-  }
-
   // Refresh Users map
   async refreshUsers(): Promise<CacheResponse> {
-    // If database was already refreshing
-    if (this.database.isRefreshing) {
+    const now = dayjs();
+
+    // If database was refreshed less than 15 minutes ago
+    if (this.database.lastRefreshTime.diff(now, 'minutes') < 15) {
       log(SM.IS_ALREADY_REFRESHING);
       return {
         status: constants.STATUS.ERROR,
@@ -60,47 +50,44 @@ class Cache {
       };
     }
 
-    // Set database as refreshing and get refresh time
-    this.database.isRefreshing = true;
-    const refreshedStartedAt: string = dayjs().format(DATE_FORMAT);
+    const refreshedStartedAt = now.format(DATE_FORMAT);
 
     // Log when refresh started
     log(SM.DATABASE_STARTED_REFRESH(refreshedStartedAt));
 
-    try {
-      // Get all users from database
-      const databaseUsers = await this.database.findAllUsers();
+    // TODO: Add correct refresh logic using Channels
+    // try {
+    //   // Get all users from database
+    //   const databaseUsers = await this.database.findAllUsers();
 
-      // Modify users with data from LeetCode
-      for (let i = 0; i < databaseUsers.length; i++) {
-        const databaseUser = databaseUsers[i];
+    //   // Modify users with data from LeetCode
+    //   for (let i = 0; i < databaseUsers.length; i++) {
+    //     const databaseUser = databaseUsers[i];
 
-        // Get username from Database User
-        const { username } = databaseUser;
+    //     // Get username from Database User
+    //     const { username } = databaseUser;
 
-        // Get data from LeetCode related to this User
-        const userData = await this.getLeetcodeDataFromUsername(username);
+    //     // Get data from LeetCode related to this User
+    //     const userData = await this.getLeetcodeDataFromUsername(username);
 
-        // If UserData was returned from Backend, replace User in cache
-        if (userData.exists) {
-          this.addOrReplaceUserInCache(username, userData);
-          log(SM.USERNAME_WAS_REFRESHED(username));
-        } else {
-          log(SM.USERNAME_WAS_NOT_REFRESHED(username));
-        }
+    //     // If UserData was returned from Backend, replace User in cache
+    //     if (userData.exists) {
+    //       this.addOrReplaceUserInCache(username, userData);
+    //       log(SM.USERNAME_WAS_REFRESHED(username));
+    //     } else {
+    //       log(SM.USERNAME_WAS_NOT_REFRESHED(username));
+    //     }
 
-        // Wait X seconds until loading next User, X is set in .env
-        await this.delay(this.delayTime);
-      }
+    //     // Wait X seconds until loading next User, X is set in .env
+    //     await this.delay(this.delayTime);
+    //   }
 
-      // Sort objects after refresh
-      this.sortUsers();
-    } catch (err) {
-      log(err.message);
-    }
+    //   // Sort objects after refresh
+    //   this.sortUsers();
+    // } catch (err) {
+    //   log(err.message);
+    // }
 
-    // Set database indicators
-    this.database.isRefreshing = false;
     const refreshFinishedAt = dayjs().format(DATE_FORMAT);
 
     // Log when refresh started
@@ -110,17 +97,6 @@ class Cache {
       status: constants.STATUS.SUCCESS,
       detail: BM.IS_REFRESHED,
     };
-  }
-
-  // Sort all Users by amount of solved questions on LeetCode
-  sortUsers(): void {
-    this.users.sort(
-      (user1, user2) => {
-        const solved1 = user1.solved !== undefined ? user1.solved : -Infinity;
-        const solved2 = user2.solved !== undefined ? user2.solved : -Infinity;
-        return solved2 - solved1;
-      },
-    );
   }
 
   // Add User by Username
@@ -196,29 +172,9 @@ class Cache {
     };
   }
 
-  // Remove all Users from Database
-  async clearUsers() {
-    const deleted = await this.database.removeAllUsers();
-
-    if (deleted) {
-      // Remove all Users from cache
-      this.users = [];
-
-      return {
-        status: constants.STATUS.SUCCESS,
-        detail: BM.DATABASE_WAS_CLEARED,
-      };
-    }
-
-    return {
-      status: constants.STATUS.ERROR,
-      detail: BM.DATABASE_WAS_NOT_CLEARED,
-    };
-  }
-
   // Load User by Username
   loadUser(username: string): User {
-    return this.users.find((user) => (
+    return this.allUsers().find((user) => (
       user.username.toLowerCase() === username.toLowerCase()
     ));
   }
