2668f558016eb7d4d9c5af59066292a54177c760
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2022-02-05 23:40:18 +0600

    Add queries to Create Channels and Channel Users tables

--- src/database/sqlite3/queries.ts
+++ src/database/sqlite3/queries.ts
@@ -5,6 +5,22 @@ export default {
       username TEXT NOT NULL
     );`
   ),
+  CREATE_CHANNELS_TABLE: (
+    `CREATE TABLE IF NOT EXISTS channels (
+      id INTEGER PRIMARY KEY,
+      chat_id TEXT NOT NULL,
+      provider TEXT NOT NULL,
+      user_limit INTEGER NOT NULL
+    );`
+  ),
+  CREATE_CHANNEL_USERS_TABLE: (
+    `CREATE TABLE IF NOT EXISTS channel_users (
+      id INTEGER PRIMARY KEY,
+      channel_id INTEGER,
+      username TEXT NOT NULL,
+      FOREIGN KEY(channel_id) REFERENCES channels(id)
+    );`
+  ),
   FIND_ALL_USERS: 'SELECT username FROM users;',
   LOAD_USER: 'SELECT username FROM users WHERE username=?;',
   ADD_USER: 'INSERT INTO users(username) VALUES (?);',
