8c421b84019f69bb474231d04ee73461414e83fe
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2022-02-05 23:32:50 +0600

    Fix issue with createButtonsFromUsers test

--- src/__tests__/chatbots/utils.test.ts
+++ src/__tests__/chatbots/utils.test.ts
@@ -3,16 +3,6 @@
 import { user1, user2 } from '../__mocks__/data.mock';
 import { mockButtonOptions } from '../__mocks__/utils.mock';
 import { createButtonsFromUsers } from '../../chatbots/utils';
-import { UserCache } from '../../cache/userCache';
-
-beforeEach(() => {
-  // Fix changed values before each test case
-  UserCache.users = new Map();
-});
-
-afterEach(() => {
-  //
-});
 
 beforeAll(async () => {
   jest.setTimeout(30000);
@@ -25,9 +15,7 @@ afterAll(async () => {
 test('chatbots.utils.createButtonsFromUsers action', async () => {
   // Valid: 2 users
   const action1 = 'action1';
-  UserCache.users.set(user1.username, user1);
-  UserCache.users.set(user2.username, user2);
-  const mockOptions1 = mockButtonOptions(action1);
+  const mockOptions1 = mockButtonOptions(action1, [user1, user2]);
   const buttonsResponse1 = createButtonsFromUsers(mockOptions1);
 
   expect(buttonsResponse1.length).toBe(2);
@@ -44,8 +32,9 @@ test('chatbots.utils.createButtonsFromUsers action', async () => {
   // Valid: 3 users
   const action2 = 'action2';
   const otherUsername = 'other_username';
-  UserCache.users.set(otherUsername, { ...user1, username: otherUsername });
-  const mockOptions2 = mockButtonOptions(action2);
+  const mockOptions2 = mockButtonOptions(
+    action2, [user1, user2, { ...user1, username: otherUsername }],
+  );
   const buttonsResponse2 = createButtonsFromUsers(mockOptions2);
 
   expect(buttonsResponse2.length).toBe(3);
@@ -53,8 +42,7 @@ test('chatbots.utils.createButtonsFromUsers action', async () => {
   // Valid: Add password
   const action3 = 'action3';
   const password3 = 'password3';
-  UserCache.users.delete(otherUsername);
-  const mockOptions3 = mockButtonOptions(action3, password3);
+  const mockOptions3 = mockButtonOptions(action3, [user1, user2], password3);
   const buttonsResponse3 = createButtonsFromUsers(mockOptions3);
 
   const user1response2 = buttonsResponse3[0];
@@ -64,8 +52,7 @@ test('chatbots.utils.createButtonsFromUsers action', async () => {
   expect(user2response2.action).toBe(`/${action3} ${user2.username} ${password3}`);
 
   // Valid: No users
-  UserCache.clear();
-  const mockOptions4 = mockButtonOptions('');
+  const mockOptions4 = mockButtonOptions('', []);
   const buttonsResponse4 = createButtonsFromUsers(mockOptions4);
 
   expect(buttonsResponse4.length).toBe(0);
