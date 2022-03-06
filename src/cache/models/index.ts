2aaa13d92c1fa852a7525c4c87ec92656e37365d
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2022-03-07 05:33:28 +0600

    Add index inside cache/models

---
+++ src/cache/models/index.ts
@@ -0,0 +1,6 @@
+import { CacheResponse, UserCacheResponse } from './response.model';
+import { ChannelData, ChannelKey, ChannelUser } from './channel.model';
+
+export {
+  CacheResponse, UserCacheResponse, ChannelData, ChannelKey, ChannelUser,
+};
