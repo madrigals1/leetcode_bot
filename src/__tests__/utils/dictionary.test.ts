65e17ca4a187c2d3bf0eff28072a11d62e47d83f
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2022-03-14 23:07:13 +0600

    Fix issue with dictionary element

--- src/__tests__/utils/dictionary.test.ts
+++ src/__tests__/utils/dictionary.test.ts
@@ -4,5 +4,5 @@ test('utils.dictionary file', () => {
   const errorMessage = 'placeholder_error_message';
 
   expect(SM.CONNECTION_STATUS.ERROR(errorMessage))
-    .toBe(`Database connection error: ${errorMessage}`);
+    .toBe(`>>> Database connection error: ${errorMessage}`);
 });
