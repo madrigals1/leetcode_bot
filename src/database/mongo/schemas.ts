9a73871d7e31341e9e58a6467cd3c7699a59f6cd
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2022-05-20 17:43:19 +0600

    Add SubscriptionModel Schema

--- src/database/mongo/schemas.ts
+++ src/database/mongo/schemas.ts
@@ -11,6 +11,12 @@ export interface IChannelModel extends mongoose.Document {
   user_limit: number;
 }
 
+export interface ISubscriptionModel extends mongoose.Document {
+  chat_id: string;
+  provider: number;
+  subscription_type: number;
+}
+
 // MongoDB collection schemas
 const userSchema = new mongoose.Schema({
   username: {
@@ -36,8 +42,25 @@ const channelSchema = new mongoose.Schema({
   },
 });
 
+const subscriptionSchema = new mongoose.Schema({
+  chat_id: {
+    type: String,
+    required: true,
+  },
+  provider: {
+    type: Number,
+    required: true,
+  },
+  subscription_type: {
+    type: Number,
+    required: true,
+  },
+});
+
 // MongoDB models
 const UserModel = mongoose.model<IUserModel>('User', userSchema);
 const ChannelModel = mongoose.model<IChannelModel>('Channel', channelSchema);
+const SubscriptionModel = mongoose
+  .model<ISubscriptionModel>('Channel', subscriptionSchema);
 
-export { UserModel, ChannelModel };
+export { UserModel, ChannelModel, SubscriptionModel };
