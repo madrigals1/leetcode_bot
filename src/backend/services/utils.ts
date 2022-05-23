0710ab0d15d9ab6143a664dbccb2fbcd2b246ec6
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2022-05-24 02:41:46 +0600

    Add error convertor

--- src/backend/services/utils.ts
+++ src/backend/services/utils.ts
@@ -1,6 +1,14 @@
-import { AxiosResponse } from 'axios';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { AxiosError, AxiosResponse } from 'axios';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function convertResponseBody(response: AxiosResponse): any {
   return response.data;
+}
+
+export function convertResponseError(error: AxiosError): any {
+  if (!error.response?.data) {
+    Promise.reject(error);
+  }
+
+  return Promise.reject(error.response.data);
 }
