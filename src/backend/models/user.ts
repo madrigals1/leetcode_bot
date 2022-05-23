5abacb61769e08a7612d8fc78ba55b47a4bc5ec5
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2022-05-24 01:04:33 +0600

    Change data from string to Object

--- src/backend/models/user.ts
+++ src/backend/models/user.ts
@@ -1,8 +1,10 @@
+import { User } from '../../leetcode/models';
+
 export interface LBBUser {
   id: number;
   url: string;
   username: string;
-  data: string;
+  data: User;
   created_at: string;
   updated_at: string;
 }
