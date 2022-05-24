55613ea535befafc9219e79a0df6f1fbf9f09d44
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2022-05-25 00:33:39 +0600

    Remove unused code

--- src/index.ts
+++ src/index.ts
@@ -3,34 +3,26 @@ import Telegram from './chatbots/telegram';
 import Discord from './chatbots/discord';
 import Slack from './chatbots/slack';
 import { refreshUsersCron } from './utils/scheduler';
-import Database from './database';
-import Cache from './cache';
+import Cache from './backend/cache';
 import { constants } from './utils/constants';
-import { delay, log } from './utils/helper';
+import { log } from './utils/helper';
 
-// Connecting to Database
-Database.connect().then(async () => {
-  // Wait 1 second for Databases to initialize
-  await delay(1000);
+Cache.preload()
+  .then(() => {
+    // Run Telegram BOT
+    if (constants.PROVIDERS.TELEGRAM.ENABLE) new Telegram().run();
 
-  // Refreshing the users
-  Cache.preload()
-    .then(() => {
-      // Run Telegram BOT
-      if (constants.PROVIDERS.TELEGRAM.ENABLE) new Telegram().run();
+    // Run Discord BOT
+    if (constants.PROVIDERS.DISCORD.ENABLE) Discord.run();
 
-      // Run Discord BOT
-      if (constants.PROVIDERS.DISCORD.ENABLE) Discord.run();
+    // Run Slack BOT
+    if (constants.PROVIDERS.SLACK.ENABLE) Slack.run();
 
-      // Run Slack BOT
-      if (constants.PROVIDERS.SLACK.ENABLE) Slack.run();
-
-      // Starting the scheduler for Cache refresher
-      refreshUsersCron();
-    })
-    .catch((err) => {
-      log(err);
-    });
-});
+    // Starting the scheduler for Cache refresher
+    refreshUsersCron();
+  })
+  .catch((err) => {
+    log(err);
+  });
 
 startRest();
