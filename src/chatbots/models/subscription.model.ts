4424323c0609f70d14099dfe4611c2782c0e033f
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2022-05-25 00:30:52 +0600

    Remove subscription model

--- src/chatbots/models/subscription.model.ts
+++ src/chatbots/models/subscription.model.ts
@@ -1,12 +1,4 @@
-import { ChannelKey } from '../../cache/models';
-
 export enum SubscriptionType {
   DailyStats = 1,
   Contest,
-}
-
-export interface Subscription {
-  id?: number;
-  channelKey: ChannelKey;
-  subscriptionType: SubscriptionType;
 }
