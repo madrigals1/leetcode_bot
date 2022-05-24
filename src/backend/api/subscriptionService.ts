2f9ef14f254201aed006b89d0dc3bcb1213f8c25
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2022-05-24 23:16:25 +0600

    Add subscriptionTypeMap in SubscriptionService

--- src/backend/api/subscriptionService.ts
+++ src/backend/api/subscriptionService.ts
@@ -1,10 +1,15 @@
+import { SubscriptionType } from '../../chatbots/models';
 import { LBBSubscription } from '../models';
 
 import { Service } from './requests';
 
 class SubscriptionService extends Service<LBBSubscription> {
+  subscriptionTypeMap = new Map<SubscriptionType, string>();
+
   constructor() {
     super('/subscriptions');
+    this.subscriptionTypeMap.set(SubscriptionType.DailyStats, '01_daily_stats');
+    this.subscriptionTypeMap.set(SubscriptionType.Contest, '02_contest');
   }
 }
 
