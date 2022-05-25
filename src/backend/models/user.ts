884e45556919b0f9a998dcc53b8fc82544b852de
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2022-05-25 23:01:25 +0600

    Add solved and solved_cml fields for User

--- src/backend/models/user.ts
+++ src/backend/models/user.ts
@@ -4,6 +4,8 @@ export interface LBBUser {
   id?: number;
   url?: string;
   username: string;
+  solved?: number;
+  solved_cml?: number;
   data?: User;
   created_at?: string;
   updated_at?: string;
