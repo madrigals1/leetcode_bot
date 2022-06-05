60473622b2b22460e5bdf4ea616d7fdbbae22b6e
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2022-06-05 22:59:36 +0600

    Remove setTimeout calls for jest

--- src/__tests__/chatbots/discord/index.deleted.ts
+++ src/__tests__/chatbots/discord/index.deleted.ts
@@ -1,13 +1,5 @@
-import { jest } from '@jest/globals';
-
 import DiscordBotInstance from '../../../chatbots/discord';
 import { ProviderMessages } from '../../../globals/messages';
-
-jest.setTimeout(30000);
-
-afterAll(async () => {
-  jest.setTimeout(5000);
-});
 
 test('chatbots.discord.index.run function', async () => {
   await DiscordBotInstance.run();
