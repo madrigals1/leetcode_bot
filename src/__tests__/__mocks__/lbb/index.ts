927bd044b1962824a01aa6d476e6b251b9b50ac4
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2022-06-05 22:51:11 +0600

    Remove nock and nock related logic

--- src/__tests__/__mocks__/lbb/index.ts
+++
@@ -1,9 +0,0 @@
-import * as nock from 'nock';
-
-import { constants } from '../../../globals/constants';
-
-import { addUserRequests } from './userService.mock';
-
-const nockInstance = nock(constants.LBB.URL);
-
-addUserRequests(nockInstance);
