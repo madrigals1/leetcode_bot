4b5e4089aebacea3221748e7cced90ebfe65c8a9
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2022-10-20 01:02:37 +0200

    Small refactoring of leetcode queries

--- src/leetcode/graphql/index.ts
+++ src/leetcode/graphql/index.ts
@@ -1,27 +1,20 @@
-import { GraphQLContext, GraphQLQuery } from '../models';
+import { GraphQLContext } from '../models';
 import { getGraphqlLink } from '../utils';
 
 export function getContestRankingContext(username: string): GraphQLContext {
-  const graphqlLink: string = getGraphqlLink();
-
-  const contestRankingQuery: GraphQLQuery = {
-    operationName: 'getContestRankingData',
-    query: `
-      query getContestRankingData($username: String!) {
-        userContestRanking(username: $username) {
-          rating
-          __typename
-        }
-      }
-    `,
-    variables: {
-      username,
-    },
-  };
-
   return {
-    link: graphqlLink,
-    query: contestRankingQuery,
+    link: getGraphqlLink(),
+    query: {
+      operationName: 'getContestRankingData',
+      query: `
+        query getContestRankingData($username: String!) {
+          userContestRanking(username: $username) {
+            rating
+          }
+        }
+      `,
+      variables: { username },
+    },
     headers: {},
   };
 }
@@ -29,144 +22,118 @@ export function getContestRankingContext(username: string): GraphQLContext {
 export function getRecentSubmissionListContext(
   username: string,
 ): GraphQLContext {
-  const graphqlLink: string = getGraphqlLink();
-
-  const recentSubmissionListQuery: GraphQLQuery = {
-    operationName: 'getRecentSubmissionList',
-    query: `
-      query getRecentSubmissionList($username: String!, $limit: Int) {
-        recentSubmissionList(username: $username, limit: $limit) {
-          title
-          titleSlug
-          time
-          statusDisplay
-          lang
-          memory
-          runtime
-          __typename
-        }
-        languageList {
-          id
-          name
-          verboseName
-          __typename
-        }
-      }
-    `,
-    variables: {
-      username,
-    },
-  };
-
   return {
-    link: graphqlLink,
-    query: recentSubmissionListQuery,
+    link: getGraphqlLink(),
+    query: {
+      operationName: 'getRecentSubmissionList',
+      query: `
+        query getRecentSubmissionList($username: String!, $limit: Int) {
+          recentSubmissionList(username: $username, limit: $limit) {
+            title
+            titleSlug
+            time
+            statusDisplay
+            lang
+            memory
+            runtime
+          }
+          languageList {
+            id
+            name
+            verboseName
+          }
+        }
+      `,
+      variables: { username },
+    },
     headers: {},
   };
 }
 
 export function getUserProfileContext(username: string): GraphQLContext {
-  const graphqlLink: string = getGraphqlLink();
-
-  const userProfileQuery: GraphQLQuery = {
-    operationName: 'getUserProfile',
-    query: `
-      query getUserProfile($username: String!) {
-        allQuestionsCount {
-          difficulty
-          count
-          __typename
-        }
-        matchedUser(username: $username) {
-          username
-          socialAccounts
-          githubUrl
-          contributions {
-            points
-            questionCount
-            testcaseCount
-            __typename
-          }
-          profile {
-            realName
-            websites
-            countryName
-            skillTags
-            company
-            school
-            starRating
-            aboutMe
-            userAvatar
-            reputation
-            ranking
-            __typename
-          }
-          submissionCalendar
-          submitStats {
-            acSubmissionNum {
-              difficulty
-              count
-              submissions
-              __typename
-            }
-            totalSubmissionNum {
-              difficulty
-              count
-              submissions
-              __typename
-            }
-            __typename
-          }
-          badges {
-            id
-            displayName
-            icon
-            creationDate
-            __typename
-          }
-          upcomingBadges {
-            name
-            icon
-            __typename
-          }
-          activeBadge {
-            id
-            __typename
-          }    __typename
-        }
-      }
-    `,
-    variables: {
-      username,
-    },
-  };
-
   return {
-    link: graphqlLink,
-    query: userProfileQuery,
+    link: getGraphqlLink(),
+    query: {
+      operationName: 'getUserProfile',
+      query: `
+        query getUserProfile($username: String!) {
+          allQuestionsCount {
+            difficulty
+            count
+          }
+          matchedUser(username: $username) {
+            username
+            socialAccounts
+            githubUrl
+            contributions {
+              points
+              questionCount
+              testcaseCount
+            }
+            profile {
+              realName
+              websites
+              countryName
+              skillTags
+              company
+              school
+              starRating
+              aboutMe
+              userAvatar
+              reputation
+              ranking
+            }
+            submissionCalendar
+            submitStats {
+              acSubmissionNum {
+                difficulty
+                count
+                submissions
+              }
+              totalSubmissionNum {
+                difficulty
+                count
+                submissions
+              }
+            }
+            badges {
+              id
+              displayName
+              icon
+              creationDate
+            }
+            upcomingBadges {
+              name
+              icon
+            }
+            activeBadge {
+              id
+            }
+          }
+        }
+      `,
+      variables: { username },
+    },
     headers: {},
   };
 }
 
 export function getLanguageStatsContext(username: string): GraphQLContext {
-  const graphqlLink: string = getGraphqlLink();
-  const userProfileQuery: GraphQLQuery = {
-    query: `
-      query languageStats($username: String!) {
-        matchedUser(username: $username) {
-          languageProblemCount {
-            languageName
-            problemsSolved
+  return {
+    link: getGraphqlLink(),
+    query: {
+      query: `
+        query languageStats($username: String!) {
+          matchedUser(username: $username) {
+            languageProblemCount {
+              languageName
+              problemsSolved
+            }
           }
         }
-      }
-    `,
-    variables: { username },
-  };
-
-  return {
-    link: graphqlLink,
-    query: userProfileQuery,
+      `,
+      variables: { username },
+    },
     headers: {},
   };
 }
