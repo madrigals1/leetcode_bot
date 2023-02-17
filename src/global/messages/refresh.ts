17955df1764d46d987a02f72717b94105bc14af4
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2023-02-18 04:06:29 +0600

    Add more refresh messages

--- src/global/messages/refresh.ts
+++ src/global/messages/refresh.ts
@@ -6,4 +6,26 @@ export class RefreshMessages {
   static startedRefresh = `${EMOJI.WAITING} Refresh was requested...`;
 
   static isRefreshed = `${EMOJI.SUCCESS} Refresh is finished`;
+
+  static cacheAlreadyRefreshed = `${EMOJI.ERROR
+  } Cache was refreshed less than 5 minutes ago`;
+
+  static cacheIsRefreshed = `${EMOJI.SUCCESS
+  } Cache was refreshed less than 5 minutes ago`;
+
+  static usernameWasRefreshed(username: string) {
+    return `${username} was refreshed`;
+  }
+
+  static usernameWasNotRefreshed(username: string) {
+    return `${username} was not refreshed`;
+  }
+
+  static databaseRequestedRefresh(time: string) {
+    return `Database requested refresh at ${time}`;
+  }
+
+  static databaseFinishedRefresh(time: string) {
+    return `Database is refreshed at ${time}`;
+  }
 }
