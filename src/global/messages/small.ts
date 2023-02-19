417e207545bfd394882ebc88e47890af5d3fd355
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2023-02-19 19:06:54 +0600

    Add 2 more messages

--- src/global/messages/small.ts
+++ src/global/messages/small.ts
@@ -1,3 +1,4 @@
+import { ChannelKey } from '../../cache/models';
 import { constants } from '../../utils/constants';
 
 const { EMOJI } = constants;
@@ -16,4 +17,14 @@ export class SmallMessages {
   static apiNotWorking = 'api_not_working';
 
   static incorrectBotType = 'Incorrect bot type';
+
+  static isConnectingTo(entity: string): string {
+    return `>>> Connecting to ${entity}`;
+  }
+
+  // Channel errors
+  static channelDoesNotExist(channelKey: ChannelKey) {
+    // eslint-disable-next-line implicit-arrow-linebreak
+    return `Channel does not exist - ${channelKey}`;
+  }
 }
