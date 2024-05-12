e7d8853f5c5d484bd8281134f80c209d35fce81a
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2024-05-12 21:00:46 +0200

    Make prometheus import less verbose

--- src/prometheus/index.ts
+++ src/prometheus/index.ts
@@ -1,6 +1,6 @@
-import * as client from 'prom-client';
+import { Histogram } from 'prom-client';
 
-const actionLogger = new client.Histogram({
+const actionLogger = new Histogram({
   name: 'leetcode_bot:action_time',
   help: 'Duration of each action in ms',
   labelNames: ['action', 'error', 'provider'],
