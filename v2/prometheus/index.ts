a059ce1138286fcabae10bd0c84447ecbccebd77
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2024-05-12 17:36:10 +0200

    Add prometheus action logger

---
+++ v2/prometheus/index.ts
@@ -0,0 +1,10 @@
+import * as client from 'prom-client';
+
+const actionLogger = new client.Histogram({
+  name: 'leetcode_bot:action_time',
+  help: 'Duration of each action in ms',
+  labelNames: ['action', 'error', 'provider'],
+  buckets: [0.1, 5, 15, 50, 100, 500],
+});
+
+export { actionLogger };
