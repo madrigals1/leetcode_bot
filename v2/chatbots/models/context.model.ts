d112234d240e82492224c5317083f7e0cc469945
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2024-05-12 21:01:18 +0200

    Make telegram not required for Context

--- v2/chatbots/models/context.model.ts
+++ v2/chatbots/models/context.model.ts
@@ -6,7 +6,7 @@ export interface Context {
   text: string;
   provider: ChatbotProvider;
   reply: (message: string, context: Context) => Promise<string>;
-  telegram: {
+  telegram?: {
     bot?: TelegramBot;
     chatId?: TelegramBot.ChatId;
   };
