0c10ed473ccd308af68e04b2ebbebe10f2d16669
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2024-05-12 17:55:34 +0200

    Add telegram bot and chatId to context

--- v2/chatbots/models/context.model.ts
+++ v2/chatbots/models/context.model.ts
@@ -1,7 +1,13 @@
+import TelegramBot from 'node-telegram-bot-api';
+
 import { ChatbotProvider } from './provider.model';
 
 export interface Context {
   text: string;
   provider: ChatbotProvider;
   reply: (message: string, context: Context) => Promise<string>;
+  telegram: {
+    bot?: TelegramBot;
+    chatId?: TelegramBot.ChatId;
+  };
 }
