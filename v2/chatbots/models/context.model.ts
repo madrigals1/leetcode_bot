73bc9cefaf4f68db86794375070cbfc0849b919e
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2024-05-12 17:39:28 +0200

    Make context even smaller

--- v2/chatbots/models/context.model.ts
+++ v2/chatbots/models/context.model.ts
@@ -1,13 +1,7 @@
-import { ArgumentManager, RequestedArgument } from '../argument';
-
 import { ChatbotProvider } from './provider.model';
 
 export interface Context {
   text: string;
-  args?: ArgumentManager;
-  reply: (message: string, context: Context) => Promise<string>;
-  argumentParser: (
-    context: Context, requestedArgs: RequestedArgument[],
-  ) => ArgumentManager;
   provider: ChatbotProvider;
+  reply: (message: string, context: Context) => Promise<string>;
 }
